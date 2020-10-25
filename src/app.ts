60582552acb75c3fc09b62adab13f77c695b9002
Eder Marques <edermarques96@gmail.com>
2020-10-25 16:12:58 -0300

    Handlers: Add handlers

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import { APP_HOST, APP_PORT } from './config.ts';
 import router from './routes/index.ts';
 
+import requestHandler from './handlers/requestHandler.ts';
+import responseHandler from './handlers/responseHandler.ts';
+
 const app = new Application();
 
+app.use(requestHandler);
 app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(responseHandler);
 
 console.log(`Listening on port ${APP_PORT}`);
 
